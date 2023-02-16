ac3861381924823d1770aec39c65313bec4a9223
alex.nunez <alex.nunez@ozona.es>
2023-02-16 15:27:17 +0100

    save prject with mvc working

---
+++ Try2 MVC/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using SignalR.Mvc;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddSignalR()
+            .AddAzureSignalR();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+         app.UseRouting();
+            app.UseFileServer();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapRazorPages();
+        app.Run();
+    }
+}
